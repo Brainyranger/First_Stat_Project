bb0a98806a15094eb2c89e51d04b18cc7a8e41b4
Brainyranger <122823004+Brainyranger@users.noreply.github.com>
2023-09-19 12:33:56 +0200

    Update Joueur.py

--- First_Stat_Project/Joueur.py
+++ First_Stat_Project/Joueur.py
@@ -19,5 +19,31 @@ class Joueur:
 
 
 
+class MonteCarlo(Joueur):
+    def __init__(self,jeu):
+        #self.etat = f"Dans le plateau {jeu.plateau.nb_ligne}x{jeu.plateau.nb_colonne},le joueur {self.j1} commence."
+        self.jeu = jeu
+
+    def play_MonteCarlo(self,joueur):
+        actions = self.jeu.colonne_disponible()
+        actions = [(action,0) for action in actions]
+
+        for i in range(1,NB_PARTI):
+            action = random.choice(actions)[0]
+            copie_jeu = Jeu(self.jeu.plateau,self.jeu.j1,self.j2)
+
+            gagnant = copie_jeu.run()
+
+            if(gagnant == joueur.id_joueur):
+                for j in range(0,len(actions)):
+                    if actions[j][0]==action:
+                        action[j][1]+=1
+
+        max_action = actions[0]
+        for i in range(1, len(actions)):
+            if (actions[i][1] > max_action[1]):
+                max_action = actions[i]
         
+        return max_action[0]
+
 
