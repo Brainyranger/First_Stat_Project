1db2c08b44d797331ba1b6aab41b5c1e26fb1ac6
Brainyranger <122823004+Brainyranger@users.noreply.github.com>
2023-09-28 17:23:02 +0200

    Add files via upload

--- First_Stat_Project/Data.py
+++ First_Stat_Project/Data.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Jeu import Jeu
-from Constante import *
+from Plateau import Plateau
+from Constante import NB_JETON,NOMBRE_LEVIERS
+from Joueur import Joueur
 from BanditManchot import BanditManchot
 from scipy.stats import kde
 
@@ -44,22 +46,23 @@ def data_run(title, joueur1, joueur2, plateau, nb_parts):
 
 
     #trace l'histogramme de la distribution du nb de victoire en fonction du nb de coup
-    plt.figure(f"{title}")
+    plt.figure(f"histogramme {title}")
     plt.hist(np.array(res_joueur1),bins=25,label="joueur 1")
     plt.hist(np.array(res_joueur2),bins=25,label="joueur 2")
     plt.ylabel("Nb victoires")
     plt.xlabel("Nb Coups")
 
     #trace le graphique de densite
+    plt.figure(f"graphique {title}")
     density_1 = kde.gaussian_kde(np.array(res_joueur1))
     density_2 = kde.gaussian_kde(np.array(res_joueur2))
     x = np.linspace(4,20,300)
     y1 = density_1(x)
     y2 = density_2(x) 
-    #plt.plot(x,y1,label="joueur 1")
-    #plt.plot(x,y2,label="joueur 2")
-    #plt.ylabel("Densité")
-    #plt.xlabel("Nombre de Coups")
+    plt.plot(x,y1,label="joueur 1")
+    plt.plot(x,y2,label="joueur 2")
+    plt.ylabel("Densité")
+    plt.xlabel("Nombre de Coups")
     
     
     plt.legend()
@@ -99,22 +102,23 @@ def data_MonteCarlo(title, joueur1, joueur2, plateau, nb_parts):
     print("Nb parties gagnées joueur 2\t:", jeu.j2.nb_parti_gagner)
 
     #trace l'histogramme de la distribution du nb de victoire en fonction du nb de coup
-    plt.figure(f"{title}")
+    plt.figure(f"histogramme {title}")
     plt.hist(np.array(res_joueur1),bins=25,label="joueur 1")
     plt.hist(np.array(res_joueur2),bins=25,label="joueur 2")
     plt.ylabel("Nb victoires")
     plt.xlabel("Nb Coups")
 
     #trace le graphique de densite
+    plt.figure(f"graphique {title}")
     density_1 = kde.gaussian_kde(np.array(res_joueur1))
     density_2 = kde.gaussian_kde(np.array(res_joueur2))
     x = np.linspace(4,20,300)
     y1 = density_1(x)
     y2 = density_2(x) 
-    #plt.plot(x,y1,label="joueur 1")
-    #plt.plot(x,y2,label="joueur 2")
-    #plt.ylabel("Densité")
-    #plt.xlabel("Nombre de Coups")
+    plt.plot(x,y1,label="joueur 1")
+    plt.plot(x,y2,label="joueur 2")
+    plt.ylabel("Densité")
+    plt.xlabel("Nombre de Coups")
     
     
     plt.legend()
@@ -153,22 +157,23 @@ def data_MonteCarlovsAleatoire(title, joueur1, joueur2, plateau, nb_parts):
     print("Nb parties gagnées joueur 2\t:", jeu.j2.nb_parti_gagner)
     
     #trace l'histogramme de la distribution du nb de victoire en fonction du nb de coup
-    plt.figure(f"{title}")
+    plt.figure(f"histogramme {title}")
     plt.hist(np.array(res_joueur1),bins=25,label="joueur 1")
     plt.hist(np.array(res_joueur2),bins=25,label="joueur 2")
     plt.ylabel("Nb victoires")
     plt.xlabel("Nb Coups")
 
      #trace le graphique de densite
+    plt.figure(f"graphique {title}")
     density_1 = kde.gaussian_kde(np.array(res_joueur1))
     density_2 = kde.gaussian_kde(np.array(res_joueur2))
     x = np.linspace(4,20,300)
     y1 = density_1(x)
     y2 = density_2(x) 
-    #plt.plot(x,y1,label="joueur 1")
-    #plt.plot(x,y2,label="joueur 2")
-    #plt.ylabel("Densité")
-    #plt.xlabel("Nombre de Coups")
+    plt.plot(x,y1,label="joueur 1")
+    plt.plot(x,y2,label="joueur 2")
+    plt.ylabel("Densité")
+    plt.xlabel("Nombre de Coups")
     
     
     plt.legend()
@@ -295,113 +300,3 @@ def data_ucb(nombre_iteration,jeu):
     plt.legend()
     plt.show()
 
-
-def data_UCTvsAleatoire(title, joueur1, joueur2, plateau, nb_parts):
-    """
-    Lance une partie entre 2 joueurs, où le joueur 1 utilise la méthode UCT pour choisir les coups,
-    et le joueur 2 joue de manière aléatoire, et étudie le nombre de coups jusqu'à une victoire, pour le joueur 1 et pour le joueur 2.
-
-    Args:
-        title (str): Le titre de l'expérience.
-        joueur1 (Joueur): Le joueur 1 (utilisant Monte Carlo).
-        joueur2 (Joueur): Le joueur 2 (jouant de manière aléatoire).
-        plateau (Plateau): L'objet Plateau représentant le plateau de jeu.
-        nb_parts (int): Le nombre de parties à jouer.
-    """
-
-    jeu = Jeu(plateau,joueur1,joueur2)
-    res_joueur1 = []
-    res_joueur2 = []
-    for i in range(0,nb_parts):
-        gagnat = jeu.run_uct_vs_alea() 
-        if jeu.gagnant == joueur1.id_joueur:
-            res_joueur1.append(NB_JETON-jeu.j1.nb_jetons)
-        if jeu.gagnant == joueur2.id_joueur:
-            res_joueur2.append(NB_JETON-jeu.j2.nb_jetons)
-        jeu.reset()
-
-
-    
-    print(title)
-    print("Nb parties nulles\t:", jeu.j1.nb_parti_egalite)
-    print("Nb parties gagnées joueur 1\t:", jeu.j1.nb_parti_gagner)
-    print("Nb parties gagnées joueur 2\t:", jeu.j2.nb_parti_gagner)
-    
-    #trace l'histogramme de la distribution du nb de victoire en fonction du nb de coup
-    plt.figure(f"{title}")
-    plt.hist(np.array(res_joueur2),bins=25,label="joueur 2")
-    plt.hist(np.array(res_joueur1),bins=25,label="joueur 1")
-    plt.ylabel("Nb victoires")
-    plt.xlabel("Nb Coups")
-    plt.show()
-
-     #trace le graphique de densite
-    density_1 = kde.gaussian_kde(np.array(res_joueur1))
-    density_2 = kde.gaussian_kde(np.array(res_joueur2))
-    x = np.linspace(4,20,300)
-    y1 = density_1(x)
-    y2 = density_2(x) 
-    plt.plot(x,y1,label="joueur 1")
-    plt.plot(x,y2,label="joueur 2")
-    plt.ylabel("Densité")
-    plt.xlabel("Nombre de Coups")
-    
-    
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-def data_UCTvsMonteCarlo(title, joueur1, joueur2, plateau, nb_parts):
-    """
-    Lance une partie entre 2 joueurs, où le joueur 1 utilise la méthode UCT pour choisir les coups,
-    et le joueur 2 utilise laméthode monte carlo, et étudie le nombre de coups jusqu'à une victoire, pour le joueur 1 et pour le joueur 2.
-
-    Args:
-        title (str): Le titre de l'expérience.
-        joueur1 (Joueur): Le joueur 1 (utilisant Monte Carlo).
-        joueur2 (Joueur): Le joueur 2 (jouant de manière aléatoire).
-        plateau (Plateau): L'objet Plateau représentant le plateau de jeu.
-        nb_parts (int): Le nombre de parties à jouer.
-    """
-
-    jeu = Jeu(plateau,joueur1,joueur2)
-    res_joueur1 = []
-    res_joueur2 = []
-    for i in range(0,nb_parts):
-        gagnat = jeu.run_uct_vs_monte_carlo() 
-        if jeu.gagnant == joueur1.id_joueur:
-            res_joueur1.append(NB_JETON-jeu.j1.nb_jetons)
-        if jeu.gagnant == joueur2.id_joueur:
-            res_joueur2.append(NB_JETON-jeu.j2.nb_jetons)
-        jeu.reset()
-
-
-    
-    print(title)
-    print("Nb parties nulles\t:", jeu.j1.nb_parti_egalite)
-    print("Nb parties gagnées joueur 1\t:", jeu.j1.nb_parti_gagner)
-    print("Nb parties gagnées joueur 2\t:", jeu.j2.nb_parti_gagner)
-    
-    #trace l'histogramme de la distribution du nb de victoire en fonction du nb de coup
-    plt.figure(f"{title}")
-    plt.hist(np.array(res_joueur2),bins=25,label="joueur 2")
-    plt.hist(np.array(res_joueur1),bins=25,label="joueur 1")
-    plt.ylabel("Nb victoires")
-    plt.xlabel("Nb Coups")
-    plt.show()
-
-     #trace le graphique de densite
-    density_1 = kde.gaussian_kde(np.array(res_joueur1))
-    density_2 = kde.gaussian_kde(np.array(res_joueur2))
-    x = np.linspace(4,20,300)
-    y1 = density_1(x)
-    y2 = density_2(x) 
-    plt.plot(x,y1,label="joueur 1")
-    plt.plot(x,y2,label="joueur 2")
-    plt.ylabel("Densité")
-    plt.xlabel("Nombre de Coups")
-    
-    
-    plt.legend()
-    plt.grid()
-    plt.show()
